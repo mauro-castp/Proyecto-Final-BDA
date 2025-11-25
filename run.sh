628d8f15e8aa14429232b45a0bbf1675220ec069
AndresG0C <andresgonzalez22004@gmail.com>
2025-11-25 00:08:16 -0600

    Actualizacion

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Agregar el directorio actual al PYTHONPATH
-export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-export FLASK_APP=app/app.py
-export FLASK_ENV=development
-
-echo "Iniciando la aplicación Flask..."
-flask run --host=0.0.0.0
